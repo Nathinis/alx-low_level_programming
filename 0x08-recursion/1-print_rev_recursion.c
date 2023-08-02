d93fb2471537d040f3dba374f7419ed13cee4198
Nathi <nkosinathisalman6@gmail.com>
2023-08-02 06:13:35 -0700

    1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - It prints the string in reverse.
+ * @s
+ */
+void _print_rev_recursion(char *s)
+{
+        if (*s)
+        {
+                _print_rev_recursion(s + 1);
+                _putchar(*s);
+        }
+}
